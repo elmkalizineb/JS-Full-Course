27137b623fda18ca6c1dfd2e42d1e3d90e330f39
elmkalizineb <elmkalizineb@gmail.com>
2025-07-08 12:13:35 +0100

    objects

---
+++ script.js
@@ -0,0 +1,25 @@
+/** 1 - variables : */
+
+let variable = 3; /** declare a variable  using let  */
+console.log(variable);
+variable = 5; // update a variable 
+
+/**  variable name can`t use special characters except $ , _ : 
+ *   let name@!#%^* =3 , let name two = 2  <-- flase declarations
+ *   let _name = 3 , let $name = 3  <-- possible 
+ */
+
+let calculation = 2 + 2;
+
+console.log(calculation);
+
+console.log(calculation + 2);
+
+let message = `hello there `; //  declare a string 
+
+const v = 3; // constant can`t be updated 
+
+console.log( typeof variable );
+console.log( typeof  message  );
+console.log( typeof  v);
+
